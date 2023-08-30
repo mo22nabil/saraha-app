f71dff8b295ed22747e529516734eb802ffdc41f
mohamed Nabil <61280396+mo22nabil@users.noreply.github.com>
2023-08-30 23:20:16 +0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connDB= ()=>{
-    return mongoose.connect(process.env.dbUrl)
+    return mongoose.connect(`mongodb+srv://route:route1234@cluster0.7gl5emz.mongodb.net/saraha`)
     .then(console.log('connected DB'))
     .catch(err =>console.log('fail to connect DB',err))
 }
